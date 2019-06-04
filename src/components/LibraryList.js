199fb469b0735b6bb983a858aafe1498d7c79710
Archer <zaib_qasim@hotmail.com>
2019-06-04 18:18:32 +0500

    Creating a list view

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,9 +1,21 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+	renderItem(library) {
+		return <ListItem library={library} />;
+	}
+
 	render() {
-		return [];
+		return (
+			<FlatList
+				data={this.props.libraries}
+				renderItem={this.renderItem}
+				keyExtractor={(library) => library.id}
+			/>
+		);
 	}
 }
 

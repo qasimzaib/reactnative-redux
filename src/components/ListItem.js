67eca866919506349615dd90042a2f2a0ff4137f
Archer <zaib_qasim@hotmail.com>
2019-06-04 18:53:14 +0500

    List view - basic

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { CardSection } from './common';
 
-class ListItem extends Component {
+class ListItem extends Component {	
 	render() {
+		const { titleStyle } = styles;
+
 		return (
 			<CardSection>
-				<Text>{this.props.library.item.title}</Text>
+				<Text style={titleStyle}>
+					{this.props.library.title}
+				</Text>
 			</CardSection>
 		);
 	}
 }
+
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+};
 
 export default ListItem;
